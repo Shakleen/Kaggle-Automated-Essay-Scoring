155d1dccce30bb2ba0f2af0dee324ea598460806
Shakleen Ishfar <shakleenishfar@gmail.com>
2024-04-26 12:32:02 -0400

    Implement dataset creation logic
    Use Dataset and DataLoader frmo PyTorch.

--- lib/config.py
+++ lib/config.py
@@ -1,4 +1,6 @@
 class Config:
+    DEBUG: bool = True
+
     # Common seed value for seeding everything.
     RANDOM_SEED: int = 29
 
@@ -7,3 +9,13 @@ class Config:
 
     # Model Name
     MODEL: str = "microsoft/deberta-v3-base"
+
+    # Max number of tokens per sequence
+    MAX_LENGTH: int = 512
+
+    # Batch Size for train and test
+    BATCH_SIZE_TRAIN: int = 1
+    BATCH_SIZE_VALID: int = 1
+
+    # For parallel processing in dataset creation
+    NUM_WORKERS: int = 6
